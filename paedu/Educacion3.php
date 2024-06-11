9818f55ee28d206abf58014dbf1afe79d5837b23
Papu3000-papa <fragosokevin01@gmail.com>
2024-06-10 21:12:55 -0600

    Add files via upload

---
+++ paedu/Educacion3.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="Css/bulma.min.css">
+    <link rel="stylesheet" href="Css/est.css">
+    <title>Ejercicios</title>
+</head>
+<body background="fondo1.png">
+    <header>
+        <h1>EJERCICIOS</h1>
+    </header><br><br>
+
+    <center>
+    <img src="ejercicios.jpg" width="700" height="900">
+</center>
+<br><br>
+<form class="box" action="Educacion4.php" Method="POST">
+    <div class="field">
+      <label class="label">Ejercicio 1:</label>
+      <div class="control">
+        <input class="input" type="text" name="ejer1" placeholder="1/4 o 1 1/4" />
+      </div>
+    </div>
+    <br>
+    <div class="field">
+      <label class="label">Ejercicio 2:</label>
+      <div class="control">
+        <input class="input" type="text" name="ejer2" placeholder="1/4 o 1 1/4" />
+      </div>
+    </div>
+    <br>
+    <div class="field">
+        <label class="label">Ejercicio 3:</label>
+        <div class="control">
+          <input class="input" type="text" name="ejer3" placeholder="1/4 o 1 1/4" />
+        </div>
+      </div>
+    <button class="button is-primary">COPMPROBAR RESPUESTAS</button>
+  </form>
+</body>
+</html>
---
+++ paedu/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="Css/bulma.min.css">
+    <link rel="stylesheet" href="Css/est.css">
+    <title>INDEX</title>
+</head>
+<body >
+    <div class="med">
+    <form class="box" action="Educacion1.php" Method="POST">
+        <div class="field">
+          <label class="label">Usuario</label>
+          <div class="control">
+            <input class="input" type="text" name="usuario" placeholder="alexexample" />
+          </div>
+        </div>
+      
+        <div class="field">
+          <label class="label">Contraseña</label>
+          <div class="control">
+            <input class="input" type="password" name="contra" placeholder="********" />
+          </div>
+        </div>
+      
+        <button class="button is-primary">Sign in</button>
+      </form>
+    </div>
+</body>
+</html>
